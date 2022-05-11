8c9ef7ef5037914c0be78550ed5a7fbb95f0056e
Joseph Anderson <jandrewanderson1995@gmail.com>
2022-05-11 09:09:44 -0500

    added route to get game by id

--- server/routes/api/game-routes.js
+++ server/routes/api/game-routes.js
@@ -3,11 +3,15 @@ const router = require('express').Router();
 // Import any controllers needed here
 const {
     createGame,
-    getAllgames
+    getAllgames,
+    addUserToGame,
+    getGameById
 } = require('../../controllers/game-contoller');
 
 // Declare the routes that point to the controllers above
 router.route('/').get(getAllgames);
 router.route('/').post(createGame);
+router.route('/:gameId').get(getGameById);
+router.route('/:gameId/users/:userId').post(addUserToGame);
 
 module.exports = router;
