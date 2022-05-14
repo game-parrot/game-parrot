b21e0cace2c874e00ac6edf34bbffd545b7602f2
Joseph Anderson <jandrewanderson1995@gmail.com>
2022-05-14 12:29:06 -0500

    fixed top game played, navbar links, and signup/login pages

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -20,27 +20,27 @@ function Login() {
     const navigate = useNavigate()
 
     const handleFormSubmit = async (e) => {
-        e.preventDefault()
-        console.log("here")
-        const query = await fetch("/api/users/auth", {
-            method: "post",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        const result = await query.json()
+        // e.preventDefault()
+        // console.log("here")
+        // const query = await fetch("/api/users/auth", {
+        //     method: "post",
+        //     body: JSON.stringify(formData),
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     }
+        // })
+        // const result = await query.json()
 
-        if (result && !result.err && result.data && result.data.token) {
-            cookie.set("auth-token", result.data.token, { expires: 3 })
-        }
+        // if (result && !result.err && result.data && result.data.token) {
+        //     cookie.set("auth-token", result.data.token, { expires: 3 })
+        // }
         navigate("/home")
     }
 
     return (
         <>
             <div className="SignLogCont test">
-                <div className="card border-info mb-3">
+                <div className="card border-info mb-3 signLogCard">
                     <div className="card-header">Login</div>
                     <div className="card-body text-info">
                         <h5 className="card-title">Enter Login Info</h5>
@@ -49,25 +49,25 @@ function Login() {
                                 <form>
 
                                     <div className="form-group row">
-                                        <label className="col-sm-2 col-form-label">Username</label>
+                                        <label className="col-sm-2 col-form-label"></label>
                                         <div className="col-sm-10">
                                             <input
                                                 type="text"
                                                 name="username"
                                                 placeholder="Username"
-                                                className="form-control"
+                                                className="form-control cardInput"
                                                 value={formData.username}
                                                 onChange={handleInputChange}
                                             />
                                         </div>
                                     </div>
                                     <div className="form-group row">
-                                        <label className="col-sm-2 col-form-label">Password</label>
+                                        <label className="col-sm-2 col-form-label"></label>
                                         <div className="col-sm-10">
                                             <input
                                                 type="password"
                                                 name="password"
-                                                className="form-control"
+                                                className="form-control cardInput"
                                                 id="inputPassword" placeholder="Password"
                                                 value={formData.password}
                                                 onChange={handleInputChange}
--- client/src/components/SignupLoginHeader.js
+++ client/src/components/SignupLoginHeader.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap';
 import '../styles/layout.css';
+import { Link } from 'react-router-dom'
 
 //display signup when signup button is clicked
 
@@ -11,20 +12,20 @@ const SignupLoginHeader = () => {
   return (
     <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/"><img className='gameparrotImg' src='https://cdn.discordapp.com/attachments/972165484269146136/973606897804537896/gameparrot.png' alt='Game Parrot'></img></a>
+                <Link className="navbar-brand" to="/"><img className='gameparrotImg' src='https://cdn.discordapp.com/attachments/972165484269146136/973606897804537896/gameparrot.png' alt='Game Parrot'></img></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end navbarNav" id="navbarNavDropdown">
                     <ul className="navbar-nav navbarUl">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#"></a>
+                            <Link className="nav-link" to="#"></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/login">Login</a>
+                            <Link className="nav-link" to="/login">Login</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/signup">Signup</a>
+                            <Link className="nav-link" to="/signup">Signup</Link>
                         </li>
                     </ul>
                 </div>
